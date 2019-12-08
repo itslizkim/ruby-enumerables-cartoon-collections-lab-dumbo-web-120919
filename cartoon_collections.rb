7c9a95b9de0be674416f10243680afaca6b1b4a5
Elizabeth Kim <elizabeth.kim93@gmail.com>
2019-12-08 17:51:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,6 @@
 def roll_call_dwarves(array)
   i = 0
   array.each_with_index {|name, index| puts "#{index + 1}. #{name}"}# code an argument here
-  # Your code here
 end
 
 def summon_captain_planet# code an argument here
